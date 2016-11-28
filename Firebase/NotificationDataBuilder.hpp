4b6d14b925340f4cce5ad065c30f33b5c2dc7af3
nicomazz_dell <nicolmazzucato@me.com>
2016-11-28 15:48:21 +0100

    small fixs

--- Firebase/NotificationDataBuilder.hpp
+++ Firebase/NotificationDataBuilder.hpp
@@ -1,9 +1,14 @@
+#include <string>
+#include "../Models/User/User.hpp"
+#include "../Models/Event/Event.hpp"
+#include "../Utility/json.hpp"
 // da un evento crea il testo per la notifica da inviare tramite firebase
+using json = nlohmann::json;
 
 class NotificationDataBuilder {
 public:
     static std::string getJsonForUserEvent(User &user, Event &e);
-
 private:
     static json getJsonDetails(Event e);
+
 };
