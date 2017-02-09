9536965dbebd2b359628af7bb43bb41d7e2a96f4
nicomazz_dell <nicolmazzucato@me.com>
2017-02-09 17:00:04 +0100

    build event updated!

--- DataSources/EventsWebProvider/INGVDataSource.cpp
+++ DataSources/EventsWebProvider/INGVDataSource.cpp
@@ -22,20 +22,27 @@ std::vector<Event> INGVDataSource::parseEvents(std::string webResponse) {
 
 //esempio di stringa:
 //10064391|2016-11-17T00:13:15.720000|43.0077|13.1222|9.4|SURVEY-INGV||||ML|3.2|--|Macerata
+//#EventID|Time|Latitude|Longitude|Depth/km|Author|Catalog|Contributor|ContributorID|MagType|Magnitude|MagAuthor|EventLocationName
+
 Event INGVDataSource::buildEvent(std::string s) {
     std::vector<std::string> splitted = split(s, '|');
-    std::string toParse;
-    for (std::string component: splitted) {
-        if (component.size())
-            toParse += component + " ";
-        else toParse += "- ";
-    }
     Event event;
-    std::stringstream ss(toParse);
+    std::stringstream ss;
     std::string id;
-    ss >> id >> event.date >> event.lat >> event.lng;
-    ss >> event.depthKm >> event.author >> event.catalog >> event.contributor >> event.contributorId >> event.magType;
-    ss >> event.magnitude >> event.author >> event.eventLocation;
+    id = splitted[0];
+    event.date = splitted[1];
+    ss = std::stringstream(splitted[2] + " " + splitted[3] + " " + splitted[4]);
+    ss >> event.lat >> event.lng >> event.depthKm;
+    event.author = splitted[5];
+    event.catalog = splitted[6];
+    event.contributor = splitted[7];
+    event.contributorId = splitted[8];
+    event.magType = splitted[9];
+    ss = std::stringstream(splitted[10]);
+    ss >> event.magnitude;
+    event.author = splitted[11];
+    event.eventLocation = splitted[12];
+
     //if the id is a string we get it hashcode (like in java)
     try { event.id = stol(id); }
     catch (std::invalid_argument e) { event.id = hashCode(id); }
